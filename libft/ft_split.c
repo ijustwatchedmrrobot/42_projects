708183ab6f1f6a38773cb5ae2acc243a5c6b9673
Said Bora Ozdamar <sozdamar@k1m32s10.42istanbul.com.tr>
2023-10-14 12:52:42 +0300

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,34 +12,80 @@
 
 #include "libft.h"
 
-char **ft_split(char const *s, char c)
+void	ft_strs_alloc(char const *s ,char c, char ***strs)
 {
-	char	**strs;
 	size_t	i;
-	size_t	j;
-	size_t	size;
+	size_t	strs_memo;
 
 	i = 0;
-	j = 0;
+	strs_memo = 0;
 	while (*(s + i))
 	{
-		size = 0;
+		if (*(s + i) != c && *(s + i + 1) == c)
+			strs_memo++;
+		i++;
+	}
+	*strs = (char **)malloc(sizeof(char *) * (strs_memo + 1));
+}
+
+void	ft_dynamic_alloc(const char* s, char c, char **strs)
+{
+	size_t	i;
+	size_t	word_size;
+	size_t	pos;
+
+	i = 0;
+	pos = 0;
+	while (*(s + pos))
+	{
+		word_size = 0;
+		while (*(s + pos) != c && *(s + pos))
+		{
+			word_size++;
+			pos++;
+		}
+		*(strs + i) = malloc(sizeof(char) * (word_size + 1));
+		i++;
+		pos++;
+	}
+}
+
+void	ft_assign(char const *s, char c, char **strs)
+{
+	size_t	i;
+	size_t	word_pos;
+	size_t	j;
+
+	i = 0;
+	word_pos = 0;
+	while (*(s + i))
+	{
+		j = 0;
 		while (*(s + i) != c)
 		{
-			size++;
+			*(*(strs + word_pos) + j) = *(s + i);
 			i++;
 		}
-		*(strs + j) = malloc((size + 1) * sizeof(char));
-		while (size)
-		{
-			*(*(strs + j) + i) = *(s + i);
-			i++;
-			size--;
-		}
-		*(*(strs + j) + i) = '\0';
-		j++;
+		*(*(strs + word_pos) + j) = '\0';
+		i++;
+		word_pos++;
 	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strs;
+	size_t	strs_memo;
+	size_t	i;
+
+	strs = 0;
+	ft_strs_alloc(s, c, &strs);
+	ft_dynamic_alloc(s, c, strs);
+	ft_assign(s, c, strs);
 	return (strs);
 }
 
-// kelimeleri ayır, size'larını al, belleği dinamik ayır
+int main()
+{
+	char **strs = ft_split("aaa bbb ccc",' ');
+}
