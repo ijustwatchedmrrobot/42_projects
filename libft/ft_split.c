9485ddced76fa384d72c65de5a0762c8cf9266b4
Said Bora Ozdamar <sozdamar@k1m31s06.42istanbul.com.tr>
2023-10-20 18:01:38 +0300

    check split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -38,3 +38,49 @@ char	**ft_split(char const *s, char c)
 {
 
 }
+
+/*
+static size_t   ft_toklen(const char *s, char c)
+{
+    size_t  ret;
+    ret = 0;
+    while (*s)
+    {
+        if (*s != c)
+        {
+            ++ret;
+            while (*s && *s != c)
+                ++s;
+        }
+        else
+            ++s;
+    }
+    return (ret);
+}
+char    **ft_split(char const *s, char c)
+{
+    char    **ret;
+    size_t  i;
+    size_t  len;
+    if (!s)
+        return (NULL);
+    i = 0;
+    ret = malloc(sizeof(char *) * (ft_toklen(s, c) + 1));
+    if (!ret)
+        return (0);
+    while (*s)
+    {
+        if (*s != c)
+        {
+            len = 0;
+            while (*s && *s != c && ++len)
+                ++s;
+            ret[i++] = ft_substr(s - len, 0, len);
+        }
+        else
+            ++s;
+    }
+    ret[i] = 0;
+    return (ret);
+}
+*/
