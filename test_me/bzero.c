53b7612f057dc5062503cb1d837f1cd0600849c1
Said Bora Ozdamar <sozdamar@k1m33s10.42istanbul.com.tr>
2023-10-22 21:35:33 +0300

    rm

--- test_me/bzero.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-void	*ft_memset(void *b, int c, size_t len);
-
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
-}
--- test_me/strchr.c
+++
@@ -1,9 +0,0 @@
-char	*strchr(const char *s, int c)
-{
-	while (*s++)
-	{
-		if (*(unsigned char *)s == (unsigned char)c)
-			return ((char *)s);
-	}
-	return 0;
-}
